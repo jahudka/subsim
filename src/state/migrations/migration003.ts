19f549f026750db25c00fa156f89b80c93f6af18
Dan Kadera <me@subsonic.cz>
2023-04-23 18:31:44 +0200

    fixes

--- src/state/migrations/migration003.ts
+++ src/state/migrations/migration003.ts
@@ -4,6 +4,8 @@ import { randomKey } from '../utils';
 export function migration003(projects: RawProject[]): RawProject[] {
   return projects.map((project) => {
     'area' in project && delete project.area;
+    'resolution' in project.simulation && delete project.simulation.resolution;
+    project.simulation.range ??= 54;
 
     for (const source of project.sources) {
       source.id ??= randomKey(project.sources);
@@ -11,6 +13,7 @@ export function migration003(projects: RawProject[]): RawProject[] {
 
     for (const guide of project.guides) {
       guide.id ??= randomKey(project.guides);
+      guide.color = guide.color.replace(/^#(.)(.)(.)$/, '#$1$1$2$2$3$3');
     }
 
     for (const variable of Object.values(project.variables)) {
