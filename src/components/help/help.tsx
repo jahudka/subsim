52580cb8b06e371ce9ee428da52d5c6d6a284508
Dan Kadera <me@subsonic.cz>
2023-05-01 23:50:50 +0200

    remove service worker for now and attempt

--- src/components/help/help.tsx
+++ src/components/help/help.tsx
@@ -1,8 +1,17 @@
 import Tooltip from 'rc-tooltip';
-import { createContext, FC, ReactElement, ReactNode, useContext, useEffect, useState } from 'react';
+import {
+  createContext,
+  FC,
+  ReactElement,
+  ReactNode,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import { HiOutlineQuestionMarkCircle } from 'react-icons/hi';
 import { BooleanField } from '../fields';
-import { Children } from '../../types';
+import { Children } from '../types';
 import { useDismissableUi } from '../hooks';
 import { tooltips, renderContent } from './tooltips';
 
@@ -19,6 +28,7 @@ export const Help: FC = () => {
   const savedState = localStorage.getItem('tooltips');
   const [active, setActive] = useState(savedState === null ? undefined : (savedState !== '0'));
   const [target, setTarget] = useState(active !== false ? 'ui.help' : undefined);
+  const touch = useRef(false);
   useDismissableUi(() => setTarget(undefined), active ?? true, '.rc-tooltip');
 
   if (active !== undefined) {
@@ -34,10 +44,12 @@ export const Help: FC = () => {
     let currentTarget: HTMLElement | undefined;
     let tmr: number | undefined;
 
-    const handleEnter = (evt: MouseEvent) => {
-      const target = (evt.target as any) as HTMLElement;
+    const checkTarget = (target?: HTMLElement | null) => {
+      if (!target) {
+        return;
+      }
 
-      if (target && target.dataset.tooltip && target.dataset.tooltip in tooltips) {
+      if (target.dataset.tooltip && target.dataset.tooltip in tooltips) {
         clearTimeout(tmr);
         currentTarget = target;
         tmr = setTimeout(() => {
@@ -50,6 +62,10 @@ export const Help: FC = () => {
       }
     };
 
+    const handleEnter = (evt: MouseEvent) => {
+      checkTarget(evt.target as any);
+    };
+
     const handleLeave = (evt: MouseEvent) => {
       if (evt.target === currentTarget) {
         tmr === undefined && setTarget(undefined);
@@ -58,16 +74,59 @@ export const Help: FC = () => {
       }
     };
 
-    document.body.addEventListener('mouseenter', handleEnter, { capture: true });
-    document.body.addEventListener('mouseleave', handleLeave, { capture: true });
+    const handleTouchStart = (evt: TouchEvent) => {
+      if (!touch.current) {
+        document.body.removeEventListener('mouseenter', handleEnter, { capture: true });
+        document.body.removeEventListener('mouseleave', handleLeave, { capture: true });
+        touch.current = true;
+
+        clearTimeout(tmr);
+        currentTarget = tmr = undefined;
+      }
+
+      if (evt.touches.length === 1) {
+        setTarget(undefined);
+        checkTarget((evt.target as any).closest('[data-tooltip]'));
+      }
+    };
+
+    const handleTouchEnd = (evt: TouchEvent) => {
+      if (evt.touches.length !== 0) {
+        return;
+      }
+
+      const target = (evt.target as any).closest('[data-tooltip]');
+
+      if (target === currentTarget) {
+        if (tmr !== undefined) {
+          clearTimeout(tmr);
+          currentTarget = tmr = undefined;
+        } else {
+          evt.preventDefault();
+        }
+      }
+    };
+
+    if (!touch.current) {
+      document.body.addEventListener('mouseenter', handleEnter, { capture: true });
+      document.body.addEventListener('mouseleave', handleLeave, { capture: true });
+    }
+
+    document.body.addEventListener('touchstart', handleTouchStart, { capture: true });
+    document.body.addEventListener('touchend', handleTouchEnd, { capture: true });
 
     return () => {
-      document.body.removeEventListener('mouseenter', handleEnter, { capture: true });
-      document.body.removeEventListener('mouseleave', handleLeave, { capture: true });
+      if (!touch.current) {
+        document.body.removeEventListener('mouseenter', handleEnter, { capture: true });
+        document.body.removeEventListener('mouseleave', handleLeave, { capture: true });
+      }
+
+      document.body.removeEventListener('touchstart', handleTouchStart, { capture: true });
+      document.body.removeEventListener('touchend', handleTouchEnd, { capture: true });
       clearTimeout(tmr);
       currentTarget = tmr = undefined;
     };
-  }, [active, setTarget]);
+  }, [active, setTarget, touch]);
 
   return (
     <HelpContext.Provider value={{ active, setActive }}>
--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,13 @@ import { App } from './components';
 
 import './layout.less';
 
-if (!/\/$/.test(location.pathname)) {
-  location.href = location.pathname.replace(/(?:\/(?:index\.html?)?)?$/, '/');
-}
-
 const container = ReactDOM.createRoot(document.getElementById('root')!);
 container.render(<App />);
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register(new URL('./service-worker.ts', import.meta.url), {
-    type: 'module',
-  }).then((sw) => sw.update());
+  navigator.serviceWorker.getRegistrations().then(async (registrations) => {
+    for (const registration of registrations) {
+      await registration.unregister();
+    }
+  });
 }
