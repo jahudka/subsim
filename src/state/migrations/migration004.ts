5b901b240d286b1a5be8c87484377c4031671353
Dan Kadera <me@subsonic.cz>
2023-06-13 21:27:19 +0200

    fix migration

--- src/state/migrations/migration004.ts
+++ src/state/migrations/migration004.ts
@@ -7,7 +7,9 @@ export function migration004(projects: RawProject[]): RawProject[] {
     }
 
     for (const variable of [...Object.values(project.variables), ...Object.values(project.globals)]) {
-      variable.step ??= 0;
+      if ('min' in variable) {
+        variable.step ??= 0;
+      }
     }
 
     return project;
